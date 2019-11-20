8f99ebed28c40ffa06eb7d5233d09774ce73f51a
Myeong <juharim@naver.com>
2019-11-20 16:33:54 +0900

    완성(1~3)
    1. 바닥, 산, 바다, 물방울 스프라이트 배치
    2. 모든 스프라이트 오른쪽에서 왼쪽으로 무한히 반복
    3. 각각의 스프라이트 속도 다르게 설정

---
+++ Assets/02.Scripts/ScrollManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollManager : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+        if (transform.position.x <= endPosition)
+            transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+    }
+}
